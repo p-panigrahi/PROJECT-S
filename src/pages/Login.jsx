55b657a8e542cb1760dd8c23def4e1e7c84d2c3e
Prabhudutta Panigrahi <58845544+p-panigrahi@users.noreply.github.com>
2024-10-07 22:21:04 +0530

    for commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 const Login = () => {
-  return;
-  <>
+  return (
     <section id="login">
-      <div className="mx-auto container px-4">
-
-        <div></div>
+      <div className="mx-auto container p-4">
+        <div className="bg-red-600 w-full max-w-md p-2 rounded-xl mx-auto">
+          login
+        </div>
       </div>
     </section>
-  </>;
+  );
 };
 
 export default Login;
